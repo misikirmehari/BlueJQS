f88eab9cd501b9b627c346b2824181b4e6158862
vicky <vicky@93b20b52-c009-4909-a2aa-4cc70c757410>
2017-12-07 18:59:06 +0000

    Added Copyright and PMD License (where appropriate)
    git-svn-id: https://gouda.msudenver.edu/cs4250/JavaProgramAnalysis/DEW/trunk@885 93b20b52-c009-4909-a2aa-4cc70c757410

--- BlueJQualitySuite/src/main/java/MenuBuilder.java
+++ BlueJQualitySuite/src/main/java/MenuBuilder.java
@@ -6,6 +6,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//BlueJQS - BlueJ Quality Tool Suite
+//Copyright (C) 2017
+//
+//This library is free software; you can redistribute it and/or
+//modify it under the terms of the GNU Lesser General Public
+//License as published by the Free Software Foundation; either
+//version 2.1 of the License, or (at your option) any later version.
+//
+//This library is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//Lesser General Public License for more details.
+//
+//You should have received a copy of the GNU Lesser General Public
+//License along with this library; if not, write to the Free Software
+//Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+//PMD's BSD-style license:
+//Copyright (c) 2003-2009, InfoEther, LLC
+//All rights reserved.
+//A full copy of the PMD license can be found in /license/PMD_license.txt
+
 /**
  * Defines the menu for the BlueJQualitySuite.
  *
